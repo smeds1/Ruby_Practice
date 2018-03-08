5d7f5d9bf026f6d862fb933c3d66d2b8e1baddc0
Sam Smedinghoff <smeds1@users.noreply.github.com>
2018-03-08 14:31:22 -0500

    Initial commit of tipCalculator.rb

---
+++ Unit1/tipCalculator.rb
@@ -0,0 +1,9 @@
+#Sam Smedinghoff
+#3/8/18
+#tipCalculator.rb
+
+puts "Price of Meal (in dollars): "
+price = gets.strip.to_f
+puts "% to tip: "
+tip = gets.strip.to_f
+puts "Your should tip " + (price*tip/100).round(2).to_s + " dollars"
