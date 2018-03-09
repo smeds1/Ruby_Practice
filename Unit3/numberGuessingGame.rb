3df83f0991c9a13dc5eb8f277be1abc97351b317
Sam Smedinghoff <smeds1@users.noreply.github.com>
2018-03-09 13:49:31 -0500

    Initial commit of numberGuessingGame.rb

---
+++ Unit3/numberGuessingGame.rb
@@ -0,0 +1,21 @@
+#Sam Smedinghoff
+#2/22/18
+#numberGuessingGame.rb
+
+num = rand(100)+1
+
+guess = 0
+numGuesses = 0
+until guess == num
+  print "Guess a number: "
+  guess = gets.to_i
+  numGuesses += 1
+  if guess > num
+    puts "Too High"
+  elsif guess < num
+    puts "Too Low"
+  end
+end
+
+puts "You win!"
+puts "It took you " + numGuesses.to_s + " guesses"
