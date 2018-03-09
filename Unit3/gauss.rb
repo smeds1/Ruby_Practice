2426ebb576bf97322167f20140ceb97bc98ef899
Sam Smedinghoff <smeds1@users.noreply.github.com>
2018-03-09 13:38:09 -0500

    Initial commit of gauss.rb

---
+++ Unit3/gauss.rb
@@ -0,0 +1,19 @@
+#Sam Smedinghoff
+#3/9/18
+#gauss.rb
+
+puts "Enter the first term: "
+t1 = gets.strip.to_f
+puts "Enter the last term: "
+t2 = gets.strip.to_f
+puts "Enter the difference between terms: "
+diff = gets.strip.to_f
+
+i = t1
+total = 0
+while i<=t2
+  total += i
+  i += diff
+end
+
+puts "The sum of the numbers from " + t1.to_s + " to " + t2.to_s + " is " + total.to_s
