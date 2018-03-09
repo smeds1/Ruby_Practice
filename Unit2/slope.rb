6d041800a4f624b47debf6c7691ffbb77b5bc74c
Sam Smedinghoff <smeds1@users.noreply.github.com>
2018-03-08 19:56:08 -0500

    Initial commit of slope.rb

---
+++ Unit2/slope.rb
@@ -0,0 +1,21 @@
+#Sam Smedinghoff
+#3/8/18
+#slope.rb - Calculates slope between two points
+
+puts "X1 = "
+x1 = gets.strip.to_f
+puts "Y1 = "
+y1 = gets.strip.to_f
+puts "X2 = "
+x2 = gets.strip.to_f
+puts "Y2 = "
+y2 = gets.strip.to_f
+
+if x2 == x1
+  puts "The slope is undefined"
+  puts "The equation of the line is X=" + x1.to_s
+else
+  slope = (y2-y1)/(x2-x1)
+  puts 'The slope is ' + slope.to_s
+  puts 'The equation of the line is y = ' + slope.to_s + 'X + ' + (y1 - x1*slope).to_s
+end
