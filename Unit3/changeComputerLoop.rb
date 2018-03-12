9fe1e6922192e2246377f5f1179dcae28c67d98b
Sam Smedinghoff <smeds1@users.noreply.github.com>
2018-03-12 13:50:38 -0400

    Initial commit of changeComputerLoop.rb

---
+++ Unit3/changeComputerLoop.rb
@@ -0,0 +1,29 @@
+#Sam Smedinghoff
+#3/12/18
+#changeComputerLoop.rb
+
+puts "Enter the number of cents you need to give in change: "
+cents = gets.to_i
+
+quarters = 0
+while cents > 25
+  quarters += 1
+  cents -= 25
+end
+
+dimes = 0
+while cents > 10
+  dimes += 1
+  cents -= 10
+end
+
+nickels = 0
+while cents > 5
+  nickels += 1
+  cents -= 5
+end
+
+puts "Quarters: #{quarters}"
+puts "Dimes #{dimes}"
+puts "Nickes: #{nickels}"
+puts "Pennies: #{cents}"
