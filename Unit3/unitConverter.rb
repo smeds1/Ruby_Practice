51836314e3a82040a2016e5f88c1c7e6827aa0bc
Sam Smedinghoff <smeds1@users.noreply.github.com>
2018-03-12 13:10:46 -0400

    Added string interpolation

--- Unit3/unitConverter.rb
+++ Unit3/unitConverter.rb
@@ -15,19 +15,19 @@ loop do
   if choice == 1
     puts "Enter number of kilometers: "
     km = gets.strip.to_f
-    puts km.to_s + " kilometers is " + (km*0.621371).round(3).to_s + ' miles '
+    puts "#{km} kilometers is #{(km*0.621371).round(3)} miles"
   elsif choice == 2
     puts "Enter number of kilograms: "
     kg = gets.strip.to_f
-    puts kg.to_s + " kilograms is " + (kg*2.20462).round(3).to_s + ' pounds '
+    puts "#{kg} kilograms is #{(kg*2.20462).round(3)} pounds"
   elsif choice == 3
     puts "Enter number of liters: "
     l = gets.strip.to_f
-    puts l.to_s + " liters is " + (l*0.264172).round(3).to_s + ' gallons '
+    puts "#{l} liters is #{(l*0.264172).round(3)} gallons"
   elsif choice == 4
     puts "Enter number of degrees celsius: "
     cel = gets.strip.to_f
-    puts cel.to_s + " degrees celsius is " + (cel*9/5+32).round(3).to_s + ' degrees fahrenheit '
+    puts "#{cel} degrees celsius is #{(cel*9/5+32).round(3)} degrees fahrenheit"
   elsif choice == 5
     break
   else
