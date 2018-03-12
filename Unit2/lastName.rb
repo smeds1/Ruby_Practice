9582b6d721f288511d819f667a5c1a1f3e73bc97
Sam Smedinghoff <smeds1@users.noreply.github.com>
2018-03-12 12:16:43 -0400

    Added string interpolation

--- Unit2/lastName.rb
+++ Unit2/lastName.rb
@@ -6,7 +6,7 @@ puts "Enter your last name: "
 name = gets.strip
 
 if name < 'N'
-  puts name + " is in the first half of the alphabet"
+  puts "#{name} is in the first half of the alphabet"
 else
-  puts name + " is in the second half of the alphabet"
+  puts "#{name} is in the second half of the alphabet"
 end
