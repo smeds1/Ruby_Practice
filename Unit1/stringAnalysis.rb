ddd3b0f7fb0a20a30bfa226fce271fe1751a2f11
Sam Smedinghoff <smeds1@users.noreply.github.com>
2018-03-12 12:30:36 -0400

    stringAnalysis.rb

--- Unit1/stringAnalysis.rb
+++ Unit1/stringAnalysis.rb
@@ -10,12 +10,12 @@ words = spaces + 1
 characters = text.length
 letters = characters - spaces
 
-puts "Your sentence has " + words.to_s + " words and " + characters.to_s + " characters and " + letters.to_s + " letters"
+puts "Your sentence has #{words} words and #{characters} characters and #{letters} letters"
 
 puts "Enter a character to search for: "
 char = gets.strip.downcase
-puts "Enter your sentence has " + text.downcase.count(char).to_s + " of the character " + char
+puts "Enter your sentence has #{text.downcase.count(char)} of the character #{char}"
 
 puts "Enter a word to search for: "
 word = gets.strip.downcase
-puts "Enter your sentence has " + text.downcase.scan(word).length.to_s + " of the word " + word
+puts "Enter your sentence has #{text.downcase.scan(word).length} of the word #{word}"
