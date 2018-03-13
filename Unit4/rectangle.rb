cc1f241d502f8639ac88e503169bb1171178b937
Sam Smedinghoff <smeds1@users.noreply.github.com>
2018-03-12 20:26:55 -0400

    Initial commit of rectangle.rb

---
+++ Unit4/rectangle.rb
@@ -0,0 +1,10 @@
+#Sam Smedinghoff
+#3/12/18
+#rectangle.rb
+
+def rectangle(length,width)
+  puts "The area is #{length*width}"
+  puts "The perimeter is #{2*length + 2*width}"
+end
+
+rectangle(3,4)
