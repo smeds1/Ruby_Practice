b5a858e2570f655ca84c874edb2c7aef37e1e8ea
Sam Smedinghoff <smeds1@users.noreply.github.com>
2018-03-13 15:43:08 -0400

    Initial commit of randomMonth.rb

---
+++ Unit5/randomMonth.rb
@@ -0,0 +1,8 @@
+#Sam Smedinghoff
+#3/13/18
+#randomMonth.rb
+
+months = ['January','February','March','April','May','June','July',
+'August','September','October','November','December']
+
+puts months[rand(12)]
